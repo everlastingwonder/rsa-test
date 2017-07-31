dc0a19cdbbd7b36e3fcb41cef0b52e7ab01f69f9
guswiedey <guswiedey@desktopmetal.com>
2017-07-31 12:22:28 -0400

    Added template structure to .c files

---
+++ encrypt.c
@@ -0,0 +1,45 @@
+/*
+*  RSA ENCRYPTOR/DECRYPTOR
+*  By Gus Wiedey
+*  v0.1.0
+*/
+
+/***************
+* PREPROCESSOR *
+*     CRAP     *
+***************/
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <ctype.h>
+// #include <math.h>
+
+// #define BLAH 69
+
+/**************
+* CUSTOM TYPE *
+* DEFINITIONS *
+**************/
+// enum BOOLEAN { false = 0, true = 1 };
+// typedef enum BOOLEAN bool;
+
+// struct MY_STRUCT {
+//   int i;
+//   float f;
+//   char c;
+// };
+// typedef struct MY_STRUCT alias;
+
+/************
+* FUNCTIONS *
+************/
+// void foo(int);
+
+int main( /*int argc, char *argv[]*/ ) {
+  // code code code
+}
+
+// void foo(int bar) {
+  // code code
+  // code code code code
+// }
